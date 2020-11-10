ae0e9f97172e5817c797ba26e783dbb4c089e996
MichaelCalabro <mmc.calabro@gmail.com>
2020-11-10 18:43:14 -0500

    sign in href and managed user states

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -3,11 +3,46 @@ import React from 'react';
 import logo from './images/liwg-logo-200x200.png'
 import LinkedInLogin from './Components/LinkedInLogin';
 import { Paper } from '@material-ui/core';
-import TemplateSelect from './Components/TemplateSelect';
+import flowStates from './FlowStates';
 
 class App extends React.Component  {
 
+  state = {
+    userState : flowStates.SIGN_IN
+  }
+
   render() {
+
+    var flowContent = null;
+
+    switch(this.state.userState) {
+      
+      case flowStates.SIGN_IN:
+        flowContent = 
+          <div>
+              <Paper className="Paper" elevation={3}> (Description) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie eleifend quam nec fermentum. 
+              Donec erat nunc, mollis eu ex sit amet, tempor scelerisque nunc. 
+              Praesent lobortis sollicitudin sollicitudin. Sed pharetra sed ligula sit amet gravida. 
+              Vestibulum pretium nulla orci, at varius lorem dictum quis.</Paper>
+
+              <LinkedInLogin></LinkedInLogin>
+          </div>
+        break;
+
+      case flowStates.AUTH_FAILED:
+        flowContent = 
+          <div>
+            <h2>There was a problem signing you in. Please try again.</h2>
+            <LinkedInLogin></LinkedInLogin>
+          </div>
+        break;
+      case flowStates.AUTH_SUCCESS:
+        //Template selection
+        break;
+
+      default:
+        flowContent = <h1>ERROR</h1>
+    }
 
     return (
       <div className="App">
@@ -15,14 +50,8 @@ class App extends React.Component  {
           <img src={logo} alt={''}/>
         </header>
 
-        <Paper className="Paper" elevation={3}> (Description) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie eleifend quam nec fermentum. 
-          Donec erat nunc, mollis eu ex sit amet, tempor scelerisque nunc. 
-          Praesent lobortis sollicitudin sollicitudin. Sed pharetra sed ligula sit amet gravida. 
-          Vestibulum pretium nulla orci, at varius lorem dictum quis.</Paper>
-
-        <TemplateSelect></TemplateSelect>
-
-        <LinkedInLogin></LinkedInLogin>
+        {flowContent}
+        
       </div>
     );
   }
--- landing-page/src/Components/LinkedInLogin.js
+++ landing-page/src/Components/LinkedInLogin.js
@@ -9,32 +9,9 @@ class LinkedInLogin extends React.Component  {
 
     return (
       <div>
-        <Button variant="contained" color="primary" startIcon={<FaLinkedin/>} onClick={() => this.callLinkedInApi()}>Sign in with LinkedIn</Button>
+        <Button variant="contained" color="primary" startIcon={<FaLinkedin/>} href="/auth/linkedin">Sign in with LinkedIn</Button>
       </div>
     );
-  }
-
-  callLinkedInApi(){
-    //alert("LinkedinLogin");
-
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    // const url = 'https://www.linkedin.com/oauth/v2/authorization' +
-    //   '?response_type=code' +
-    //   '&client_id=78c63g5t25dr9l' + 
-    //   '&redirect_uri=http://127.0.0.1:3000/auth/linkedin/callback' + 
-    //   '&scope=r_liteprofile';
-
-
-    // fetch(proxyurl + url)
-    // .then(res => res.text())
-    // .then((html) => {
-    //   var parser = new DOMParser();
-    //   var doc = parser.parseFromString(html, "text/html");
-    //   console.log(doc);
-
-    // })
-    // .catch(console.log)
-
   }
 }
   
