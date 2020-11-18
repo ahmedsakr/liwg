89659a0ee7b6696eeecfe4802b98736fa31c5a1e
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2020-11-18 17:01:34 -0500

    Use Promises instead of callbacks

--- backend/template-generation/generate-source-file.js
+++ backend/template-generation/generate-source-file.js
@@ -1,7 +1,6 @@
 let Handlebars = require('handlebars');
 let fs = require('fs');
 const filePath = process.env.TEMPLATE_PATH || __dirname + '/test/';
-const logger = process.env.DEVELOPMENT || false;
 
 function readFile(file) {
     return new Promise((resolve, reject) => {
@@ -25,19 +24,16 @@ function readFile(file) {
  *  Note: data.template is a required attribue of the JSON object
  */
 exports.convertTemplate = function (data) {
-    if (!data.template) console.log('Error: No template provided.')
-    readFile(filePath + data.template).then(template => { 
-        if (logger) {
-            console.log('Template ===> ', template);
-            console.log('Data ===> ', JSON.stringify(data));
-        }
+    return new Promise((resolve, reject) => {
+        if (!data.template) reject('No template provided.')
+        readFile(filePath + data.template).then(template => { 
+            template = Handlebars.compile(template);
+            result = template(data);
 
-        template = Handlebars.compile(template);
-        result = template(data);
-        if (logger) console.log('Conversion Result ===> ', result);
-
-        fs.writeFile(filePath + 'generated-file.js', result, function (err) {
-            if (err) throw err;
+            fs.writeFile(filePath + 'generated-file.js', result, function (err) {
+                if (err) reject(err);
+                resolve(filePath + 'generated-file.js')
+            });
         });
     });
 }
