a8b440ced5e4e73f3360503b52779c044fbe683f
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2020-11-19 22:25:00 -0500

    Initial implementation of server operations for compressing files

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
-var cors = require('cors');
-let converter = require('../scripts/generate-source-file.js');
+const cors = require('cors');
+const converter = require('../scripts/generate-source-file.js');
+const liwgFs = require('../scripts/compress-source-files.js');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,19 +15,20 @@ app.use(
     })
 );
 
-app.post('/generate-file', (request, response) => {
-    converter.convertTemplate(request.body)
-    .then(file => {
-        response.download(file, (err) => {
-            if (err) {
-                console.log('Error downloading file: ', err);
-            }
-        });
-    })
-    .catch(error => {
+app.post('/generate-file', async (request, response) => {
+    let dirName = uuidv4();
+    const { body } = request;
+    try {
+        await liwgFs.createDirectory('dist/' + dirName);
+        await liwgFs.copyTemplateFiles('./dist/' + dirName + '/' + body.template, '../test/' + body.template);
+        await converter.convertTemplate(body, './dist/' + dirName + '/' + body.template);
+        await liwgFs.compressDirectory('dist/' + dirName, 'dist/' + dirName + '.zip');
+        await liwgFs.removeDirectory('dist/' + dirName);
+        response.download('./dist/' + dirName + '.zip', 'website-template.zip');
+    } catch (error) {
         console.log('Error: ', error);
         response.json({ error: error });
-    })
+    }
 });
 
 app.listen(port, () => {
