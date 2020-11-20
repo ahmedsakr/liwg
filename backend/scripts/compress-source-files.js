f75654a88f5685f5faed2eb505b2a59b88fe3b22
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2020-11-19 22:23:16 -0500

    Initial implementation of LIWG fs operations

--- backend/scripts/compress-source-files.js
+++ backend/scripts/compress-source-files.js
@@ -1,30 +1,65 @@
+/**
+ * A collection of LIWG specific file system operations
+ */
+
 var archiver = require('archiver');
 var fs = require('fs');
+var rimraf = require("rimraf");
 
 exports.createDistDirectory = function () {
     let dir = './test/dist';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 }
 
+exports.copyTemplateFiles = function (dirName, template) {
+    return new Promise((resolve, reject) => {
+        fs.copyFile(template, dirName, (error) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve()
+            }
+        });
+    });
+}
+
+exports.removeDirectory = function (dirName) {
+    return new Promise((resolve, reject) => {
+        rimraf(dirName, (error) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve()
+            }
+        });
+    });
+}
+
 exports.createDirectory = function (dirName) {
-    if (!fs.existsSync(dirName)){
-        fs.mkdirSync(dirName);
-    }
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName)
+            resolve()
+        } else {
+            reject('Directory already exists');
+        }
+    });
+
 }
 
 exports.compressDirectory = function (src, out) {
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  const stream = fs.createWriteStream(out);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    const stream = fs.createWriteStream(out);
 
-  return new Promise((resolve, reject) => {
-    archive
-    .directory(src, false)
-    .on('error', err => reject(err))
-    .pipe(stream);
+    return new Promise((resolve, reject) => {
+        archive
+        .directory(src, false)
+        .on('error', err => reject(err))
+        .pipe(stream);
 
-    stream.on('close', () => resolve());
-    archive.finalize();
-  });
+        stream.on('close', () => resolve());
+        archive.finalize();
+    });
 }
