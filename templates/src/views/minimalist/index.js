1697f761e4700e10e501aed17e71ba6c8fef95c4
Ahmed Sakr <ahmed@sakr.ca>
2020-11-21 17:35:06 -0500

    finish template with sidebar

--- templates/src/views/minimalist/index.js
+++ templates/src/views/minimalist/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import Profile from './content/Profile';
-import { Grid, withStyles, Divider } from '@material-ui/core';
+import { Grid, withStyles, Divider, Hidden } from '@material-ui/core';
+import { LinkedIn } from '@material-ui/icons';
 import CurrentJob from './content/CurrentJob';
 import Education from './content/Education';
 
 const styles = {
     root: {
         minHeight: "100vh",
-        padding: "2em",
         backgroundColor: '#f0f0f0'
+    },
+    sideContent: {
+        backgroundColor: '#242424',
+        color: 'white',
+        textAlign: 'center',
+        '& > h1': {
+            margin: 0
+        },
+        '& > h3': {
+            fontWeight: 'normal',
+            margin: 0,
+            marginBottom: '1rem'
+        },
+        '& > p': {
+            border: "solid 1px white",
+            borderRadius: "6px",
+            color: 'white',
+            background: "#404040",
+            fontSize: "0.8rem",
+            padding: "5px",
+            display: 'flex',
+            alignItems: 'center',
+            cursor: 'pointer'
+        }
     },
     content: {
         padding: '1rem',
-        borderRadius: '10px'
+        borderRadius: '10px',
+        marginLeft: '8.3%'
     },
     divider: {
         margin: "20px 0px",
@@ -26,7 +51,15 @@ const Minimalist = (props) => {
 
     return (
         <>
-            <Grid container justify="center" className={classes.root}>
+            <Grid container className={classes.root}>
+                <Hidden smDown>
+                    <Grid container direction="column" alignItems="center" justify="center" sm={4} className={classes.sideContent}>
+                        <h1>Thanks for dropping by.</h1>
+                        <h3>Let's get to know each other.</h3>
+                        <p>Connect on LinkedIn <LinkedIn  /></p>
+                    </Grid>
+                </Hidden>
+
                 <Grid container direction="column" sm={8} md={6} className={classes.content}>
                     <Profile />
 
