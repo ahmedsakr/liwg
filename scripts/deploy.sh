9d6a81aebe352d3e8f10eb5f9d733d93fbc26045
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 23:01:12 -0500

    remove irrelevanr stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
-echo 'Running deploy.sh script'
 set -x
 tar -czf package.tgz ../code && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./untar.sh
-echo 'Finished running deploy.sh script'
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
-echo 'Running untar.sh script'
 set -x
-# export NODE_ENV=production
 cd /var/app/liwg && \
 tar zxvf package.tgz -C . && \
 cd code/backend
 npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log &
 cd ../landing-page
 npm run frontend  > ../../logs/frontend.log 2> ../../logs/frontend_err.log &
-echo 'Finished running untar.sh script'
