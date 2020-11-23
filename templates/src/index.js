f8f3478f48bdd162d73ee6eaed4a7b3812aa9b66
Ahmed Sakr <ahmed@sakr.ca>
2020-11-22 19:52:10 -0500

    First version of a minimal template (#13)

---
+++ templates/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Minimalist from './views/minimalist';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Minimalist />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
