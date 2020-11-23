6f5e6eb43ac967254945215541af09a444541bb9
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 22:32:07 -0500

    update running scripts to run the application

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,8 @@ set -x
 # export NODE_ENV=production
 cd /var/app/liwg && \
 tar zxvf package.tgz -C . && \
-ls
-cd build
-ls
+cd code/backend
+npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log 
+cd ../landing-page
+npm run frontend  > ../../logs/frontend.log 2> ../../logs/frontend_err.log
 echo 'Finished running untar.sh script'
