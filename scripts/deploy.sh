d77313ee99ef6295720678390197a8303d50f58d
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 19:05:10 -0500

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
-!/usr/bin/env sh
+#!/usr/bin/env sh
+echo 'Running deploy.sh script'
 set -x
 tar -czf package.tgz build && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+echo 'Finished running deploy.sh script'
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+echo 'Running untar.sh script'
 set -x
 # export NODE_ENV=production
 cd $REMOTE_APP_DIR && \
@@ -5,3 +7,4 @@ tar zxvf package.tgz -C . && \
 ls
 cd build
 ls
+echo 'Finished running untar.sh script'
