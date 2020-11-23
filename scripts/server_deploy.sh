cdbc3be9e1a39b48e4d55b5811e97de6412fb4cf
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-23 00:28:15 -0500

    update

--- scripts/server_deploy.sh
+++ scripts/server_deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 set -x
-ls
 tar -czf package.tgz code && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./untar.sh
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < scripts/untar.sh
