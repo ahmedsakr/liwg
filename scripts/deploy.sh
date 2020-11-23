7b39c837ad56f7d89dd22243d1ac45ee329383a3
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 23:17:04 -0500

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-set -x
-tar -czf package.tgz ../code && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./untar.sh
+# if [[ $TRAVIS_BRANCH == 'main' ]]; then
+if [[ $TRAVIS_BRANCH == 'main' || $TRAVIS_BRANCH == 'ci_cd_liwg' ]]; then
+    set -x
+    tar -czf package.tgz ../code && \
+    scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+    ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./untar.sh
+fi
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -2,6 +2,8 @@
 set -x
 cd /var/app/liwg && \
 tar zxvf package.tgz -C . && \
+fuser -k 5000/tcp
+fuser -k 3000/tcp
 cd code/backend
 npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log &
 cd ../landing-page
