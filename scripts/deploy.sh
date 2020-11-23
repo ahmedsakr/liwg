3addfaff332e64c9512339e120171d20e6c8d4a0
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 20:48:26 -0500

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 echo 'Running deploy.sh script'
 set -x
+cd ..
+ls
 tar -czf package.tgz ../../liwg && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
