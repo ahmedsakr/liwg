bd9f5ab5718d4d7d7fdb2278d2987eb4b8ffae23
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 23:26:51 -0500

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-# if [[ $TRAVIS_BRANCH == 'main' ]]; then
-if [ $TRAVIS_BRANCH == 'main'  ] || [ $TRAVIS_BRANCH == 'ci_cd_liwg' ] ; then
+if [[ $TRAVIS_BRANCH == 'ci_cd_liwg' ]]; then
+# if [ $TRAVIS_BRANCH == 'main'  ] || [ $TRAVIS_BRANCH == 'ci_cd_liwg' ] ; then
     set -x
     tar -czf package.tgz ../code && \
     scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
