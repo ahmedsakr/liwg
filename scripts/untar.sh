71ce214af639f311bae206e5efccd128cf0a0f22
Arsii <arsalan.sadiq.1996@gmail.com>
2020-11-22 22:46:29 -0500

    run commands as background processes

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -5,7 +5,7 @@ set -x
 cd /var/app/liwg && \
 tar zxvf package.tgz -C . && \
 cd code/backend
-npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log 
+npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log &
 cd ../landing-page
-npm run frontend  > ../../logs/frontend.log 2> ../../logs/frontend_err.log
+npm run frontend  > ../../logs/frontend.log 2> ../../logs/frontend_err.log &
 echo 'Finished running untar.sh script'
