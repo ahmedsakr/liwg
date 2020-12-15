0109c6b826d92d1521586097f9a60f7815e7466b
MichaelCalabro <37020895+MichaelCalabro@users.noreply.github.com>
2020-12-14 20:43:04 -0500

    Template selection (#16)
    * material ui makeStyles
    
    * updated styles and example link on template card
    
    * minor code spacing change

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './images/liwg-logo-200x200.png'
 import LinkedInLogin from './Components/LinkedInLogin';
+import TemplateSelect from './Components/TemplateSelect'
 import { Paper } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import flowStates from './FlowStates';
 
-const styles = {
+const styles = makeStyles({
 
     app: {
       textAlign: 'center',
@@ -24,65 +25,61 @@ const styles = {
       margin: '16px',
       display: 'inline-block'
     },
-};
+});
 
-class App extends React.Component  {
+const App = () => {
 
-  state = {
-    userState : flowStates.SIGN_IN
-  }
+  const classes  = styles();
+  const [userState, setUserState] = useState(flowStates.AUTH_SUCCESS);
 
-  componentDidMount(){
+  useEffect(() => {
     document.body.style.backgroundColor = '#e0d9cd';
+  }, []);
+
+  var flowContent = null;
+
+  switch(userState) {
+    
+    case flowStates.SIGN_IN:
+      flowContent = 
+        <div>
+            <Paper className={classes.paper} elevation={3}> (Description) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie eleifend quam nec fermentum. 
+            Donec erat nunc, mollis eu ex sit amet, tempor scelerisque nunc. 
+            Praesent lobortis sollicitudin sollicitudin. Sed pharetra sed ligula sit amet gravida. 
+            Vestibulum pretium nulla orci, at varius lorem dictum quis.</Paper>
+
+            <LinkedInLogin/>
+        </div>
+      break;
+
+    case flowStates.AUTH_FAILED:
+      flowContent = 
+        <div>
+          <h2>There was a problem signing you in. Please try again.</h2>
+          <LinkedInLogin/>
+        </div>
+      break;
+    case flowStates.AUTH_SUCCESS:
+      //Template selection
+      flowContent = <TemplateSelect/>
+      break;
+
+    default:
+      flowContent = <h1>ERROR</h1>
   }
 
-  render() {
+  return (
+    <div className={classes.app}>
+      <header className={classes.appHeader}>        
+        <img src={logo} alt={''}/>
+      </header>
 
-    const { classes } = this.props;
-
-    var flowContent = null;
-
-    switch(this.state.userState) {
+      {flowContent}
       
-      case flowStates.SIGN_IN:
-        flowContent = 
-          <div>
-              <Paper className={classes.paper} elevation={3}> (Description) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie eleifend quam nec fermentum. 
-              Donec erat nunc, mollis eu ex sit amet, tempor scelerisque nunc. 
-              Praesent lobortis sollicitudin sollicitudin. Sed pharetra sed ligula sit amet gravida. 
-              Vestibulum pretium nulla orci, at varius lorem dictum quis.</Paper>
-
-              <LinkedInLogin/>
-          </div>
-        break;
-
-      case flowStates.AUTH_FAILED:
-        flowContent = 
-          <div>
-            <h2>There was a problem signing you in. Please try again.</h2>
-            <LinkedInLogin></LinkedInLogin>
-          </div>
-        break;
-      case flowStates.AUTH_SUCCESS:
-        //Template selection
-        break;
-
-      default:
-        flowContent = <h1>ERROR</h1>
-    }
-
-    return (
-      <div className={classes.app}>
-        <header className={classes.appHeader}>        
-          <img src={logo} alt={''}/>
-        </header>
-
-        {flowContent}
-        
-      </div>
-    );
-  }
+    </div>
+  );
+  
 
 }
 
-export default withStyles(styles)(App);
+export default App;
--- landing-page/src/Components/LinkedInLogin.js
+++ landing-page/src/Components/LinkedInLogin.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { FaLinkedin } from 'react-icons/fa';
 
-class LinkedInLogin extends React.Component  {
+const LinkedInLogin = () => {
 
-  render() {
-
-    return (
-      <div>
-        <Button variant="contained" color="primary" startIcon={<FaLinkedin/>} href="http://localhost:5000/auth/linkedin/login">Sign in with LinkedIn</Button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Button variant="contained" color="primary" startIcon={<FaLinkedin/>} href="http://localhost:5000/auth/linkedin/login">Sign in with LinkedIn</Button>
+    </div>
+  );
+  
 }
   
 export default LinkedInLogin;
