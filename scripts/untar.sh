fea910604ce92828f5751fb5bdfbbb13635eceac
Arsii <arsalan.sadiq.1996@gmail.com>
2020-12-26 14:18:55 -0500

    Req Changes: Updated All Changes

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ cd /var/app/liwg && \
 tar zxvf package.tgz -C . && \
 fuser -k 5000/tcp
 fuser -k 3000/tcp
-cd code/backend
-npm run backend > ../../logs/backend.log 2> ../../logs/backend_err.log &
+cd backend
+npm i && npm start > ../../logs/backend.log 2> ../../logs/backend_err.log &
 cd ../landing-page
-npm run frontend  > ../../logs/frontend.log 2> ../../logs/frontend_err.log &
+npm i && npm start   > ../../logs/frontend.log 2> ../../logs/frontend_err.log &
