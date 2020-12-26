a1005870ee2840dcf12b85a87d00bc227b839f84
Arsii <arsalan.sadiq.1996@gmail.com>
2020-12-26 15:18:04 -0500

    update logs

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -5,6 +5,6 @@ tar zxvf package.tgz -C . && \
 fuser -k 5000/tcp
 fuser -k 3000/tcp
 cd backend
-npm i && npm run start > ../../logs/backend.log 2> ../../logs/backend_err.log &
+npm i && npm run start > ../logs/backend.log 2> ../logs/backend_err.log &
 cd ../landing-page
-npm i && npm run start   > ../../logs/frontend.log 2> ../../logs/frontend_err.log &
+npm i && npm run start   > ../logs/frontend.log 2> ../logs/frontend_err.log &
