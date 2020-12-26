278b917a0cd5206ce687374fcaa786ac4109d845
Arsii <arsalan.sadiq.1996@gmail.com>
2020-12-26 14:52:39 -0500

    Add .env file and update tar packaging

--- scripts/server_deploy.sh
+++ scripts/server_deploy.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env sh
 
 set -x
-tar -czf package.tgz code && \
+
+CLIENT_ID=${CLIENT_ID}
+CLIENT_SECRET=${CLIENT_SECRET}
+LINKEDIN_CALLBACK=${LINKEDIN_CALLBACK}
+LINKEDIN_SUCCESS=${LINKEDIN_SUCCESS}
+LINKEDIN_FAILED=${LINKEDIN_FAILED}
+
+cat <<EOF > .env
+CLIENT_ID=${CLIENT_ID}
+CLIENT_SECRET=${CLIENT_SECRET}
+LINKEDIN_CALLBACK=${LINKEDIN_CALLBACK}
+LINKEDIN_SUCCESS=${LINKEDIN_SUCCESS}
+LINKEDIN_FAILED=${LINKEDIN_FAILED}
+EOF
+
+mv .env /backend/
+
+tar -czf package.tgz code ../backend ../templates ../backend && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < scripts/untar.sh
